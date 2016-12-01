ccd725f1db691ae4b06ddfc379b59b6ac7714ebe
Simon Fish <simon@undyingfish.com>
2016-12-01 20:36:49 +0000

    Add error handling for unrecognised IDs

--- main.rb
+++ main.rb
@@ -3,9 +3,13 @@ File.open("smashpoint.txt").each do |line|
   input = URI.encode(line)
   code = line.split("/")
   code = code[code.length-1].chomp
-  filename = "#{code}.brstm"
-  puts filename
-  open(filename, 'wb') do |file|
-    file << open(URI.encode("http://smashcustommusic.com/brstm/" + code)).read
+  begin
+    filename = "#{code}.brstm"
+    puts filename
+    open(filename, 'wb') do |file|
+      file << open(URI.encode("http://smashcustommusic.com/brstm/" + code)).read
+    end
+  rescue
+    puts "Not found."
   end
 end
