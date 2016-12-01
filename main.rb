f4d94e573d60ed8cc67a2977aa9828e1f15d2f0f
Stephen Foulds <stephen.foulds@gmail.com>
2016-12-01 22:48:34 +0000

    Rename file with new filename

--- main.rb
+++ main.rb
@@ -5,14 +5,16 @@ File.open("smashpoint.txt").each do |line|
   code = code[code.length-1].chomp
   begin
     filename = "#{code}.brstm"
+    new_filename = ''
     raise "NaN" unless code.to_i>0
     open(filename, 'wb') do |file|
       open(URI.encode("http://smashcustommusic.com/brstm/" + code)) do |uri|
         fileinfo =  uri.metas["content-disposition"][0]
-        puts fileinfo[/(?<=filename=")[^\"]+/]
+        new_filename = fileinfo[/(?<=filename=")[^\"]+/]
         file.write(uri.read)
       end
     end
+    File.rename(filename, new_filename)
   rescue
     puts "Not found - must be a number or link that ends with one."
   end
